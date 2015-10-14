356302812375d19ee52be5576094ddf4a95e59bf
Guoqing <yugq@fenqi.im>
2015-10-14 16:12:42 +0800

    Add comments of another method to calculate plaincode.

--- src/Vol1/_1006_Untwist.java
+++ src/Vol1/_1006_Untwist.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 // Take care of the newline symbol.
 // The formula is ciphercode[i] = (plaincode[ki mod n] - i) mod 28. 
+// Another method to calculate plainCode[(i*k)%len]=(cipherCode[i]+i)%28.   
 public class _1006_Untwist {
 	private static Scanner in;
 
